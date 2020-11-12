9295745a3f1bf90c42fd1dd9cb1e873858202d3e
wajogonz <wajogonz@espol.edu.ec>
2020-11-11 22:29:07 -0500

    Agregar ParteB
    Agregando ParteB

---
+++ Taller03-SOLID/Taller03SOLID/src/seccionB/Notificacion.java
@@ -0,0 +1,13 @@
+package seccionB;
+
+public class Notificacion {
+	private int tipoNotificacion;
+	public void notificar(Pago pago) {
+		if(tipoNotificacion==1) {
+			//enviarEmail();
+		}
+		else {
+			//enviarSMS();
+		}
+	}
+}
