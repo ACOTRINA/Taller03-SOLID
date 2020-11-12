c5cad651155371faec966e808d7d9fe786bbe20c
sgloayza <sgloayza@espol.edu.ec>
2020-11-11 22:52:38 -0500

    Commit Sandra Loayza

--- Taller03-SOLID/Taller03SOLID/src/seccionB/Notificacion.java
+++ Taller03-SOLID/Taller03SOLID/src/seccionB/Notificacion.java
@@ -1,13 +1,13 @@
 package seccionB;
 
 public class Notificacion {
-	private int tipoNotificacion;
-	public void notificar(Pago pago) {
-		if(tipoNotificacion==1) {
-			//enviarEmail();
-		}
-		else {
-			//enviarSMS();
-		}
-	}
+    private int tipoNotificacion;
+    public void notificar(Pago pago) {
+            if(tipoNotificacion==1) {
+                    //enviarEmail();
+            }
+            else {
+                    //enviarSMS();
+            }
+    }
 }
