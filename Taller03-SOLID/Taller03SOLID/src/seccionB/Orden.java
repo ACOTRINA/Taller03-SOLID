0fe98700eed29d3e8c62c9c148974a958adbc8f1
acotrina <acotrina@espol.edu.ec>
2020-11-11 23:01:29 -0500

    clase orden - Arlette Cotrina

---
+++ Taller03-SOLID/Taller03SOLID/src/seccionB/Orden.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seccionB;
+
+import java.util.List;
+
+/**
+ *
+ * @author lolac
+ */
+public class Orden {
+    private List articulos;
+    
+    public void agregarArticulo(Articulo articulo){
+        //Agregar un artículo a la compra
+    }
+    
+    public void removerArticulo(Articulo articulo){
+        //Remover un articulo a la compra
+    }
+    
+}
