001ce2e821f149323beede58861957de33fb1c0b
rpcedill <rpcedill@espol.edu.ec>
2020-11-12 11:36:54 -0500

    nuevo edit B

--- Taller03-SOLID/Taller03SOLID/src/seccionB/Notificacion.java
+++ Taller03-SOLID/Taller03SOLID/src/seccionB/Notificacion.java
@@ -1,13 +1,7 @@
 package seccionB;
 
 public class Notificacion {
-    private int tipoNotificacion;
     public void notificar(IPago pago) {
-            if(tipoNotificacion==1) {
-                    //enviarEmail();
-            }
-            else {
-                    //enviarSMS();
-            }
+            //enviarEmail();
     }
 }
--- Taller03-SOLID/Taller03SOLID/src/seccionB/Pago.java
+++ Taller03-SOLID/Taller03SOLID/src/seccionB/Pago.java
@@ -2,7 +2,8 @@ package seccionB;
 
 public class Pago implements IPago{
     //atributos aqui
-    
+    Factura fact;
+     
     public void realizarCobro (double monto) {
         //cargar el monto de compra al medio de pago
     }
